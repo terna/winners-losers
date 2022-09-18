f32579ce61d698840e114c9841825276aedfb05f
Pietro Terna <pietro.terna@unito.it>
2022-09-18 18:44:13 +0200

    studying MPI II

--- MPIexperimentUsingNumpy.py
+++ MPIexperimentUsingNumpy.py
@@ -1,2 +1,37 @@
 
 # MPIexperimentUsingNumpy.py
+
+import numpy as np
+import random
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+procid = comm.Get_rank()
+nprocs = comm.Get_size()
+if nprocs<2:
+    print("C'mon, get real....")
+    sys.exit(1)
+
+random.seed(procid)
+random_bound = nprocs*nprocs
+random_number = random.randint(1,random_bound)
+#print("[%d] random=%d" % (procid,random_number),flush=True)
+
+# native mode send
+#print("a",procid,random_number,flush=True)
+max_random = comm.allreduce(random_number,op=MPI.MAX) # max over all procid
+#print("b",procid,max_random,flush=True)
+
+if procid==0:
+    print("Python native:\n max=%d" % max_random,flush=True)
+
+myrandom = np.empty(1,dtype=int) #unitilized
+myrandom[0] = random_number
+allrandom = np.empty(nprocs,dtype=int)
+
+# numpy mode send
+comm.Allreduce(myrandom,allrandom[:1],op=MPI.MAX)
+print(procid,allrandom,flush=True)
+
+if procid==0:
+    print("Python numpy:\n max=%d" % allrandom[0],flush=True)
